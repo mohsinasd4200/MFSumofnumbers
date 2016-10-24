7dcbe509448b81670b94fe597facc4b8014a5399
Student P_10 <studentp_10@Felixs-MacBook-Pro.local>
2016-10-24 11:57:10 +0530

    feeling excited

--- MFSumofnumbers/main.m
+++ MFSumofnumbers/main.m
@@ -6,12 +6,25 @@
 //  Copyright © 2016 Mohsin Farooqui. All rights reserved.
 //
 
+
+void sumofnumbers(int,int,int);
+
 #import <Foundation/Foundation.h>
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        int a,b,c;
+        printf("Enter the three number\n");
+        scanf("%d%d%d",&a,&b,&c);
+        sumofnumbers(a,b,c);
+        
     }
     return 0;
+}
+
+void sumofnumbers(int x,int y,int z)
+{
+    int s;
+    s=x+y+z;
+    printf("The sum of three numbers is %d\n",s);
 }
